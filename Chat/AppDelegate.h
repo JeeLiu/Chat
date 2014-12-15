6243036ec26410c65e99399211c601e839b12195
bluesky <724432662@qq.com>
2014-12-15 20:24:55 +0800

    倒入初始空项目

---
+++ Chat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chat
+//
+//  Created by sky on 14/12/15.
+//  Copyright (c) 2014年 sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chat
+//
+//  Created by sky on 14/12/15.
+//  Copyright (c) 2014年 sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
